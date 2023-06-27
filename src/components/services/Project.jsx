940d5192b48ccbebe32c70f1ca2522fb659989cc
ayshaasif <ayshaasif14@gmail.com>
2023-06-27 15:44:48 +0400

    updated the project thumbnails

--- src/components/services/Project.jsx
+++ src/components/services/Project.jsx
@@ -4,11 +4,14 @@ import React from 'react'
 const Project = (props)=>{
     return(
         <div className='project__card'>
+
+
             <div className='card__thumbnail'>
                 <img className='Project__Image' src={props.ProjectImage}/>
             </div>
+
             <div className='card-info'>
-                <h5>{props.ProjectName}</h5>
+                <h5><a href={props.link}>{props.ProjectName}</a></h5>
                 <p>{props.ProjectDetail}</p>
             </div>
             
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './services.css'
 import Project from './Project.jsx'
 import Candy from '../../assets/candy-crush-saga-candies.png'
+import SkillSetWebsite from '../../assets/SkillsetSearchform.jpg'
+import JavaSql from '../../assets/Java-Mysql.jpg'
+import DjangoCrud from '../../assets/django-crud.jpg'
+import DSprojects from '../../assets/DSProjects.png'
+
 const Services = () => {
 
     let projectDetail_student_record = `Built using PHP MySQL. This project was developed to ease the 
@@ -11,13 +16,25 @@ const Services = () => {
     ,Developed using Java GUI integrated with  MySQL Database set up using XAMPP Server.`
 
     let projectDetail_Candy_crush = `developed the famous Candy Crush using Vanilla Javascript, HTML,CSS`
+
+
+    let links = [
+        'https://github.com/ayshaasif/FirstProject',
+        'https://github.com/ayshaasif/VaccinationSystem.git',
+        '',
+        'https://github.com/ayshaasif/DjangoChatApp',
+        'https://github.com/ayshaasif/SKILL-SET-WEBSITE'
+    ]
     return (
         <section id="services">
             <h4>Projects</h4>
             <div className="container">
-              <Project ProjectName="Candy Crush" ProjectImage={Candy} ProjectDetail={projectDetail_Candy_crush} />  
-              <Project ProjectName="Student Record Management System " ProjectImage={Candy} ProjectDetail= {projectDetail_student_record} />  
-              <Project ProjectName="Vaccination Booking System" ProjectImage={Candy} ProjectDetail={projectDetail_vaccination_booking_system} />  
+              <Project ProjectName="Web Development Projects - Candy Crush" link= {links[0]} ProjectImage={Candy} ProjectDetail={projectDetail_Candy_crush} />  
+              <Project ProjectName="Vaccination Booking System" link= {links[1]} ProjectImage={JavaSql} ProjectDetail={projectDetail_vaccination_booking_system} />  
+              <Project ProjectName="Data Science Projects " link= {links[2]} ProjectImage={DSprojects} ProjectDetail= {projectDetail_student_record} />  
+              <Project ProjectName="Django Projects " link= {links[3]} ProjectImage={DjangoCrud} ProjectDetail= {projectDetail_student_record} />  
+              <Project ProjectName="Student Record Management System " link= {links[4]}  ProjectImage={SkillSetWebsite} ProjectDetail= {projectDetail_student_record} />  
+
             </div>
         </section>
     )
