26fbe7c0e6bf9de3ce85204a557ea1e74210e928
ayshaasif <ayshaasif14@gmail.com>
2023-06-28 08:36:37 +0400

    updating to deploy

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,35 +6,45 @@ import SkillSetWebsite from '../../assets/SkillsetSearchform.jpg'
 import JavaSql from '../../assets/Java-Mysql.jpg'
 import DjangoCrud from '../../assets/django-crud.jpg'
 import DSprojects from '../../assets/DSProjects.png'
-
+import cryptsio from '../../assets/cryptsio.png'
 const Services = () => {
+   
+    let projectDetails = [
+        `developed the famous Candy Crush using Vanilla Javascript, HTML,CSS`,
 
-    let projectDetail_student_record = `Built using PHP MySQL. This project was developed to ease the 
-    efforts of the faculty to filter students based on their skills.`
+        `Vaccination booking system for managemingappointments ,pre booking of vaccine, 
+                        using Java GUI integrated with  MySQL , XAMPP Server`,
 
-    let projectDetail_vaccination_booking_system = `Built a vaccination booking system to facilate management of appointments and pre booking of 2nd dose of vaccine 
-    ,Developed using Java GUI integrated with  MySQL Database set up using XAMPP Server.`
+        `MediaPipe - POSE DETECTION
+        DeepFake detection using Transfer learning - Python - TensorFlow, PyTorch, Matplotlib, seaborn, pandas`,
 
-    let projectDetail_Candy_crush = `developed the famous Candy Crush using Vanilla Javascript, HTML,CSS`
+        `Django based Applications, Chat Portal using Django-channels , Django Rest Framework APIs`,
 
+        `Built using PHP MySQL. This project was developed to ease the 
+                    efforts of the faculty to filter students based on their skills`,
+
+        `Etherium - blockchain Android Application 
+        send etheriums from one account to another , view your transactions, connected to Google Firebase`
+    ]
 
     let links = [
         'https://github.com/ayshaasif/FirstProject',
         'https://github.com/ayshaasif/VaccinationSystem.git',
         '',
         'https://github.com/ayshaasif/DjangoChatApp',
-        'https://github.com/ayshaasif/SKILL-SET-WEBSITE'
+        'https://github.com/ayshaasif/SKILL-SET-WEBSITE',
+        'https://github.com/ayshaasif/Cryptsio'
     ]
     return (
         <section id="services">
-            <h4>Projects</h4>
+            <h4><u>Projects</u></h4>
             <div className="container">
-              <Project ProjectName="Web Development Projects - Candy Crush" link= {links[0]} ProjectImage={Candy} ProjectDetail={projectDetail_Candy_crush} />  
-              <Project ProjectName="Vaccination Booking System" link= {links[1]} ProjectImage={JavaSql} ProjectDetail={projectDetail_vaccination_booking_system} />  
-              <Project ProjectName="Data Science Projects " link= {links[2]} ProjectImage={DSprojects} ProjectDetail= {projectDetail_student_record} />  
-              <Project ProjectName="Django Projects " link= {links[3]} ProjectImage={DjangoCrud} ProjectDetail= {projectDetail_student_record} />  
-              <Project ProjectName="Student Record Management System " link= {links[4]}  ProjectImage={SkillSetWebsite} ProjectDetail= {projectDetail_student_record} />  
-
+              <Project ProjectName="Web Development Projects - Candy Crush" link= {links[0]} ProjectImage={Candy} ProjectDetail={projectDetails[0]} />  
+              <Project ProjectName="Vaccination Booking System" link= {links[1]} ProjectImage={JavaSql} ProjectDetail={projectDetails[1]} />  
+              <Project ProjectName="Data Science Projects " link= {links[2]} ProjectImage={DSprojects} ProjectDetail={projectDetails[2]}/>  
+              <Project ProjectName="Django Projects " link= {links[3]} ProjectImage={DjangoCrud} ProjectDetail={projectDetails[3]} />  
+              <Project ProjectName="Student Record Management System " link= {links[4]}  ProjectImage={SkillSetWebsite} ProjectDetail={projectDetails[4]} />  
+              <Project ProjectName="CRYPTSIO" link= {links[5]}  ProjectImage={cryptsio} ProjectDetail={projectDetails[5]}/>  
             </div>
         </section>
     )
